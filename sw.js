cfa4e6a816b8520f34324e79686e00dfb4754722
Abubakar Jamil <abubakar.jamil@gmail.com>
2026-02-15 01:13:31 +0500

    add al brooks price action terms sort game files

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = 'brooks-termsort-v1';
+const ASSETS = ['/', '/index.html', '/icon-192.png', '/icon-512.png', '/icon.svg', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
